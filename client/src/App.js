5d9265e76ad71bbb8005d33b6f00bcb6bd903354
Maxime Mondini <mh-mondini@hotmail.com>
2023-05-04 21:12:43 -0400

    init project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Amazing Shop</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
