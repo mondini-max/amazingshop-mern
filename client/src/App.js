34fb50638ac1eea79156f80eaaa8cd15ac1cc04b
Maxime Mondini <mh-mondini@hotmail.com>
2023-05-04 21:47:06 -0400

    create data.js , import product and display products list

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { data } from './data/data';
 
 function App() {
   return (
@@ -6,7 +7,29 @@ function App() {
       <header>
         <a href='/'>Amazing Shop</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {data.products.map((product) => {
+            return (
+              <div className='product' key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className='product-info'>
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ client/src/data/data.js
@@ -0,0 +1,57 @@
+import p1 from '../assets/images/p1.jpg';
+import p2 from '../assets/images/p2.jpg';
+import p3 from '../assets/images/p3.jpg';
+import p4 from '../assets/images/p4.jpg';
+
+export const data = {
+  products: [
+    {
+      name: 'Nike slim t-shirt',
+      slug: 'nike-slim-t-shirt',
+      category: 'Shirts',
+      image: p1,
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: p2,
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: p3,
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: p4,
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
